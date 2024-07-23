e817cc199d1375bce61454c3f07bf0894360a54e
Maxim <maxtab1@mail.ru>
2024-07-23 21:00:38 +0300

    regular

---
+++ js/main.js
@@ -0,0 +1,75 @@
+const log = console.log;
+
+// function timeRemaining(targetDate) {
+// 	const now = new Date();
+
+// 	const diffMs = targetDate - now;
+
+// 	if (diffMs <= 0) {
+// 		return {
+// 			days: 0,
+// 			hours: 0,
+// 			minutes: 0,
+// 			seconds: 0,
+// 		};
+// 	}
+
+// 	const diffDays = Math.floor(diffMs / (1000 * 60 * 60 * 24));
+// 	const diffHours = Math.floor(
+// 		(diffMs % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+// 	);
+// 	const diffMinutes = Math.floor((diffMs % (1000 * 60 * 60)) / (1000 * 60));
+// 	const diffSeconds = Math.floor((diffMs % (1000 * 60)) / 1000);
+// 	const diffMilliseconds = diffMs % 1000;
+
+// 	return {
+// 		days: diffDays,
+// 		hours: diffHours,
+// 		minutes: diffMinutes,
+// 		seconds: diffSeconds,
+// 	};
+// }
+
+// const futureDate = new Date("2024-12-31T23:59:59.999");
+// const remainingTime = timeRemaining(futureDate);
+// console.log(
+// 	`Time remaining: ${remainingTime.days} days, ${remainingTime.hours} hours, ${remainingTime.minutes} minutes, ${remainingTime.seconds} seconds, and ${remainingTime.milliseconds} milliseconds.`
+// );
+// *******************************************************************************
+function timeRemaining(targetDate) {
+	const now = new Date();
+	const diffMs = targetDate - now;
+
+	if (diffMs <= 0) {
+		return {
+			days: 0,
+			hours: 0,
+			minutes: 0,
+			seconds: 0,
+		};
+	}
+
+	const diffDays = Math.floor(diffMs / (1000 * 60 * 60 * 24));
+	const diffHours = Math.floor(
+		(diffMs % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+	);
+	const diffMinutes = Math.floor((diffMs % (1000 * 60 * 60)) / (1000 * 60));
+	const diffSeconds = Math.floor((diffMs % (1000 * 60)) / 1000);
+
+	return {
+		days: diffDays,
+		hours: diffHours,
+		minutes: diffMinutes,
+		seconds: diffSeconds,
+	};
+}
+
+function updateTimer() {
+	const futureDate = new Date("2024-08-10T09:29:59");
+	const remainingTime = timeRemaining(futureDate);
+
+	const timerElement = document.getElementById("timer");
+	timerElement.textContent = `До начала: ${remainingTime.days} дней, ${remainingTime.hours} часов, ${remainingTime.minutes} минут, ${remainingTime.seconds} секунд`;
+}
+
+setInterval(updateTimer, 1);
